5a91c235e84a3c12952522be3c0a1092cd60fbe6
Faisal Ridwan Siregar <faisalridwansiregar@gmail.com>
2019-04-09 09:08:41 +0700

    by faisal

--- application/views/v_page-header.php
+++ application/views/v_page-header.php
@@ -9,7 +9,7 @@
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link rel="shortcut icon" href="<?= base_url('assets/img/maskot.png'); ?>">
-    <title>SiCepat</title>
+    <title>SiCepatv</title>
 
     <!-- My CSS -->
     <link rel="stylesheet" href="assets/css/header_style.css">
